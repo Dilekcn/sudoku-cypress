5916c3d536b3fab9e294d7231b6f95dccca0a2f8
Dilekcn <canturkdk@gmail.com>
2022-09-20 10:10:20 +0100

    timer

--- sudoku-master/cypress/e2e/timer.cy.ts
+++ sudoku-master/cypress/e2e/timer.cy.ts
@@ -1,5 +1,5 @@
 /// <reference types="Cypress" />
-describe('Timer', () => {
+describe('Show ', () => {
     it('show 10 sec',() => {
       cy.visit('/')
       cy.contains('.status__time','00:10', {timeout: 11_000})
